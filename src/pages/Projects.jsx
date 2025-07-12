9d57f7423879f00d9172bb21971790f53ff6d95e
Nofal <adelfarizi182@gmail.com>
2025-07-12 18:46:36 +0700

    project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,25 +11,14 @@ const Projects = () => (
     <div className="bg-white dark:bg-gray-800 dark:border dark:border-gray-700 p-8 rounded-2xl shadow-sm">
         <div className="pb-8">
             <h2 className="text-3xl font-bold text-gray-900 dark:text-white">Projects</h2>
-            <p className="text-gray-500 dark:text-gray-400 mt-1">Several projects that I have worked on, both private and open source.</p>
+            <p className="text-gray-500 dark:text-gray-400 mt-1">Several projects that I have worked on.</p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {portfolioData.projects.map(project => (
                 <Link to={`/projects/${project.slug}`} key={project.id} className="group block">
                     <div className="relative rounded-xl overflow-hidden mb-4">
                         <img src={project.image} alt={`Gambar Proyek ${project.title}`} className="w-full h-50 transform group-hover:scale-105 transition-transform duration-300" />
-                        {project.featured && (
-                            <div className="absolute top-3 right-3 bg-blue-500 text-white text-xs font-semibold px-2.5 py-1 rounded-full flex items-center gap-1">
-                                <Star size={12} className="inline-block" />
-                                <span>Featured</span>
-                            </div>
-                        )}
-                        {project.login && (
-                             <div className="absolute top-3 right-3 bg-gray-700 text-white text-xs font-semibold px-2.5 py-1 rounded-full flex items-center gap-1">
-                                <Lock size={12} className="inline-block" />
-                                <span>Login</span>
-                            </div>
-                        )}
+
                     </div>
                     <h3 className="text-xl font-bold text-gray-900 dark:text-white">{project.title}</h3>
                     <p className="text-gray-600 dark:text-gray-400 mt-2 text-sm leading-relaxed">{project.description}</p>
